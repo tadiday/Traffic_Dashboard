a39adf93ca58129543341c7e3a3564878d470c17
Yelsrow <125905503+Yelsrow@users.noreply.github.com>
2024-09-27 14:46:22 -0400

    Update

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,11 +1,52 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
 
-app.use(express.static("public"))
+// Create a sql connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootpass',
+  database: 'traffic_visual'
+});
+// Connect to the DB
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database!');
+});
 
-app.get('/', (req, res) => {
-    console.log("Got")
-    res.status(200).json({message: "You got em"})
-})
+app.get('/api/select', (req, res) => {
+    var selectQuery = 'SELECT * FROM text_files WHERE user_id = ?';
+    var userId = 1;
 
-app.listen(3000)
+    connection.query(selectQuery, [userId], (err, results) => {
+        if (err) throw err;
+        console.log('Records found:', results);
+    });
+
+    console.log("Selected");
+});
+
+
+app.get('/api/insert', (req, res) => {
+
+    console.log("Inserting");
+    res.status(200).json({message: "You got em"});
+});
+
+app.listen(3000, () => {
+    console.log('Server.js App is listening on port 3000');
+});
+
+// Closes DB Connections on receiving end signals
+const shutdown = () => {
+    connection.end((err) => {
+        if (err) throw err;
+        console.log('Database Connection closed.');
+    });
+    console.log('Closed out remaining connections');
+    process.exit(0);
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
