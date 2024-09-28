804228f6191a46cdcf468d7391cdc2c2e6da40df
Yelsrow <125905503+Yelsrow@users.noreply.github.com>
2024-09-28 18:00:19 -0400

    file upload works!

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql');
+const multer = require('multer');
+const cors = require('cors');
+// Used for uploading a file, though its not stored locally with this config
+const upload = multer();
+
+// Enable CORS so no complaints with FE & BE communication
+app.use(cors());
 
 // Create a sql connection
 const connection = mysql.createConnection({
@@ -9,12 +16,14 @@ const connection = mysql.createConnection({
   password: 'rootpass',
   database: 'traffic_visual'
 });
+
 // Connect to the DB
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to the database!');
 });
 
+// Test Select Query
 app.get('/api/select', (req, res) => {
     var selectQuery = 'SELECT * FROM text_files WHERE user_id = ?';
     var userId = 1;
@@ -27,11 +36,25 @@ app.get('/api/select', (req, res) => {
     console.log("Selected");
 });
 
+// Upload file Query
+app.post('/api/upload', upload.single('file'),(req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    //console.log('File received:', req.file);
+    
+    const fileData = req.file.buffer.toString('utf-8');
 
-app.get('/api/insert', (req, res) => {
+    var InsertQuery = "INSERT INTO text_files (file_type, file_name, user_id, file_content, collection_id) VALUES (?, ?, 1, ?, 1)";
+    connection.query(InsertQuery, [req.body.fileType, req.file.originalname,  fileData], (error, results) => {
+        if (error) {
+            console.error('Error inserting data:', error);
+            return res.status(500).send('Error inserting data.');
+        }
 
-    console.log("Inserting");
-    res.status(200).json({message: "You got em"});
+        res.send('File data inserted successfully.');
+        console.log('File data inserted successfully.');
+    });
 });
 
 app.listen(3000, () => {
@@ -43,9 +66,8 @@ const shutdown = () => {
     connection.end((err) => {
         if (err) throw err;
         console.log('Database Connection closed.');
+        process.exit(0);
     });
-    console.log('Closed out remaining connections');
-    process.exit(0);
 };
 
 process.on('SIGTERM', shutdown);
--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,28 +1,57 @@
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   // Model 
   const [file, setFile] = useState(null);
+  const [fileType, setFileType] = useState('summary');
 
-  function upload(file){
+  const upload = async () => {
     console.log("Upload button clicked");
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log("File Type: ", fileType);
+    formData.append('fileType', fileType);
+
+    try {
+        const response = await axios.post('http://localhost:3000/api/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        console.log('File uploaded successfully:', response.data);
+    } catch (error) {
+        console.error('Error uploading file:', error);
+    }
   };
 
   // Controller
-
-  function handleFileChange(file){
-    ;
+  function handleFileChange(e){
+    setFile(e.target.files[0]);
   };
 
   function handleUpload(){
     upload();
   };
 
-  // View
+  function handleFileTypeChange(e){
+    setFileType(e.target.value);
+  };
 
+  // View
   return (
     <div>
+      <select onChange={handleFileTypeChange}>
+        <option value='summary'>Summary</option>
+        <option value='file10'>File 10</option>
+        <option value='file11'>File 11</option>
+        <option value='file12'>File 12</option>
+        <option value='file13'>File 13</option>
+        <option value='file14'>File 14</option>
+        <option value='file15'>File 15</option>
+        <option value='file16'>File 16</option>
+      </select>
       <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload</button>
     </div>
